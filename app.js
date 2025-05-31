e549574335dcdf712fc38e898c65bd8a059a29e8
Tushar Sharma <tusharsharma.wfa@gmail.com>
2025-05-31 17:01:54 +0530

    Updates

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.use(compression());
 
 // Rate limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 25 * 60 * 1000, // 15 minutes
+    max: 1000, // limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.'
 });
 
